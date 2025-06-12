20c8986736d1452a13c172a249287cb20f5d6445
sojeong132 <cleo708028@gmail.com>
2025-06-12 17:01:13 +0900

    feat: 20250612 todoList done

--- react-study/src/TodoList/components/IndexLayout/IndexLayout.jsx
+++ react-study/src/TodoList/components/IndexLayout/IndexLayout.jsx
@@ -3,10 +3,10 @@ import IndexHeader from '../IndexHeader/IndexHeader';
 import * as s from './styles';
 import React from 'react';
 
-function IndexLayout({children}) {
+function IndexLayout({filter, setFilter, setSearchText, children}) {
     return (
         <div css={s.layout}>
-            <IndexHeader />
+            <IndexHeader filter={filter} setFilter={setFilter} setSearchText={setSearchText}/>
             {children}
         </div>
     );
