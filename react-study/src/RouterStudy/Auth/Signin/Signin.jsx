65dea9d0e4d5796b4676d6e86a7b37a43eed3d52
sojeong132 <cleo708028@gmail.com>
2025-06-18 17:46:44 +0900

    feat: 20250618 로그인 완료 수정

--- react-study/src/RouterStudy/Auth/Signin/Signin.jsx
+++ react-study/src/RouterStudy/Auth/Signin/Signin.jsx
@@ -174,7 +174,11 @@ function Signin(props) {
         })
         
         try{
-            await axios.post(url, data);
+            const response = await axios.post(url, data);
+            const accessToken = response.data?.accessToken;
+            if(!!accessToken) {
+                localStorage.setItem("AccessToken", accessToken);
+            }
             alert("로그인 완료");
         } catch(error) {
             alert("로그인 오류");
